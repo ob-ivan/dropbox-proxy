616ce03f3e3aa91bfbe3af762551ed85af2a9386
ob-ivan <ob-ivan@github.com>
2013-09-05 22:29:23 +0400

    Понял наконец, как надо получить accessToken и куда его сохранить

--- index.php
+++ index.php
@@ -12,8 +12,9 @@ require_once $docroot . '/vendor/autoload.php';
  *
  * Config is a json file reposing at config.json in the document root.
  *  {
- *      debug   : Boolean which should be true only in development environment.
- *      storage : Path to the distributed files' directory relative to the document root.
+ *      accessToken : String identifying client requests.
+ *      debug       : Boolean which should be true only in development environment.
+ *      storage     : Path to the distributed files' directory relative to the document root.
  *  }
 **/
 $configFilename = $docroot . '/config.json';
@@ -30,19 +31,29 @@ if ($config['debug']) {
 
 // Additional services //
 
+// TODO: Encapsulate these in a DropboxServiceProvider and publish it.
+$app['dropbox.access_token'] = $app->share(function () use ($app) {
+    if (! isset($app['config']['accessToken'])) {
+        /**
+         * TODO: Output the instruction.
+         *  - Go to /dropbox-auth-start
+         *  - Get access code and store it to config.json.
+         *  - Go to /dropbox-auth-finish
+         *  - Get accessToken and store it to config.json.
+         *  - Done.
+        **/
+        /**
+         * TODO: Automate the process.
+        **/
+        throw new Exception('No accessToken is set in config.');
+    }
+    return $app['config']['accessToken'];
+});
 $app['dropbox.app_info'] = $app->share(function () use ($app) {
     return Dropbox\AppInfo::loadFromJsonFile($app['docroot'] . '/dropbox.json');
 });
 $app['dropbox.client'] = $app->share(function () use ($app) {
-    try {
-        // TODO: Понять, откуда брать код авторизации, потому что пока что
-        // эксперимент показывает, что он одноразовый.
-        list($accessToken, $userId) = $app['dropbox.web_auth']->finish($app['config']['code']);
-    }
-    catch (Dropbox\Exception $e) {
-        throw new Exception('Error communicating with Dropbox API: ' . $e->getMessage(), 0, $e);
-    }
-    return new Dropbox\Client($accessToken, $app['dropbox.client_identifier']);
+    return new Dropbox\Client($app['dropbox.access_token'], $app['dropbox.client_identifier']);
 });
 $app['dropbox.client_identifier'] = 'DownloadProxy/0.1';
 $app['dropbox.web_auth'] = $app->share(function () use ($app) {
@@ -58,6 +69,17 @@ $app->get('/dropbox-auth-start', function () use ($app) {
     // Redirect to Dropbox page and generate an authorization token.
     return $app->redirect($app['dropbox.web_auth']->start());
 })->bind('dropbox-auth-start');
+
+$app->get('/dropbox-auth-finish', function () use ($app) {
+    try {
+        // Authorization code can be used only once to obtain accessToken.
+        list($accessToken, $userId) = $app['dropbox.web_auth']->finish($app['config']['code']);
+    }
+    catch (Dropbox\Exception $e) {
+        throw new Exception('Error communicating with Dropbox API: ' . $e->getMessage(), 0, $e);
+    }
+    return $accessToken;
+})->bind('dropbox-auth-finish');
 
 $app->get('/{file}', function ($file) use ($app) {
     // Download a file.
