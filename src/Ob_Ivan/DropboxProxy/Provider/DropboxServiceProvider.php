4e44baf6b1e01a4c09ab4f78084745542f03aad4
ob-ivan <ob-ivan@github.com>
2013-09-07 00:40:11 +0400

    Add comments on services that will be eventually provided by DropboxServiceProvider

--- src/Ob_Ivan/DropboxProxy/Provider/DropboxServiceProvider.php
+++ src/Ob_Ivan/DropboxProxy/Provider/DropboxServiceProvider.php
@@ -16,12 +16,32 @@
  *
  *  - [dropbox.client_identifier]
  *      Your application name and a version which will be used to form User-Agent
- *      header while making API requests. Refer to Dropbox API docs for details.
+ *      header while making API requests with [dropbox.client] service.
+ *      Refer to Dropbox API docs for details on how to choose a proper value.
  *
  * Services:
  *  - [dropbox.access_token]
+ *      A string value used to prove your app's identity while making requests.
+ *      This is a service because its default behavior is to lookup for
+ *      [dropbox.auth_code] parameter and call [dropbox.web_auth] service
+ *      to obtain the access token value. Note that in this case it is your
+ *      responsibility to store the access token value, or else the auth code
+ *      value will be wasted.
+ *      Once you have stored access token value you can substitute this service
+ *      with its value in your application:
+ *          $app['dropbox.access_token'] = file_get_contents('access_token.txt');
+ *
  *  - [dropbox.app_info]
+ *      An instance of Dropbox\AppInfo class which look ups [dropbox.app_info.json]
+ *      file and reads it contents. It is used by [dropbox.web_auth] only, so
+ *      you can ignore it if you have already stored the access token value.
+ *
  *  - [dropbox.client]
+ *      An instance of Dropbox\Client class which you call to make actual API
+ *      requests. It requires [dropbox.client_identifier] to be defined and
+ *      [dropbox.access_token] to be valid (obtained during OAuth2 authoriztion
+ *      process and unexpired), otherwise exceptions will be thrown.
+ *
  *  - [dropbox.web_auth]
  *
  * Workflow
