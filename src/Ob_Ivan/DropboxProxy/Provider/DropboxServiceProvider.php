e7b9547ccba098bd9931eefd53e5ab0dfb9d5c8e
ob-ivan <ob-ivan@github.com>
2013-09-07 00:24:33 +0400

    Add abundant comment on DropboxServiceProvider class

--- src/Ob_Ivan/DropboxProxy/Provider/DropboxServiceProvider.php
+++ src/Ob_Ivan/DropboxProxy/Provider/DropboxServiceProvider.php
@@ -1,4 +1,31 @@
 <?php
+/**
+ * Provider for a set of services under [dropbox.] namespace
+ * in a Silex application.
+ *
+ * Parameters:
+ *  - [dropbox.auth_code]
+ *      An authorization code received from Dropbox website when you call
+ *      [dropbox.web_auth]->start(). It can be used only once to obtain
+ *      and store [dropbox.access_token] value.
+ *
+ *  - [dropbox.app_info.json]
+ *      Absolute path to a json file with your app credentials (key and secret).
+ *      Used by [dropbox.app_info] service only. May be omitted if you already
+ *      have [dropbox.access_token] value stored.
+ *
+ *  - [dropbox.client_identifier]
+ *      Your application name and a version which will be used to form User-Agent
+ *      header while making API requests. Refer to Dropbox API docs for details.
+ *
+ * Services:
+ *  - [dropbox.access_token]
+ *  - [dropbox.app_info]
+ *  - [dropbox.client]
+ *  - [dropbox.web_auth]
+ *
+ * Workflow
+**/
 namespace Ob_Ivan\DropboxProxy\Provider;
 
 use Silex\Application;
