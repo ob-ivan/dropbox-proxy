a995d617ca0566e5932ce5410be616143f2d3b13
ob-ivan <ob-ivan@github.com>
2013-09-18 22:51:42 +0400

    Try to instantiate native Memcache class and realize I won't be satisfied with it

--- src/Ob_Ivan/DropboxProxy/ResourceProvider/MemcacheResourceProvider.php
+++ src/Ob_Ivan/DropboxProxy/ResourceProvider/MemcacheResourceProvider.php
@@ -21,5 +21,20 @@ class MemcacheResourceProvider implements ResourceProviderInterface
 {
     function populate(ResourceContainer $container)
     {
+        $container->register('memcache', function ($container) {
+            $memcache = new Memcache();
+            if (isset($container['memcache.unix_socket'])) {
+                $memcache->addServer(
+                    'unix://' . $container['memcache.unix_socket'],
+                    0
+                );
+            } elseif (isset($container['memcache.host']) && isset($container['memcache.port'])) {
+                $memcache->addServer(
+                    $container['memcache.host'],
+                    $container['memcache.port']
+                );
+            }
+            // TODO: Set namespace.
+        });
     }
 }
