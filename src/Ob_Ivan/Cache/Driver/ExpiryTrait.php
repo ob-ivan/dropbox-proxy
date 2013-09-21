ef7b09d08550fdc4d600e5c1239355cc69e01874
ob-ivan <ob-ivan@github.com>
2013-09-21 19:31:31 +0400

    Create trait to simplify implementation of new expiry argument semantics

---
+++ src/Ob_Ivan/Cache/Driver/ExpiryTrait.php
@@ -0,0 +1,24 @@
+<?php
+namespace Ob_Ivan\Cache\Driver;
+
+trait ExpiryTrait
+{
+    protected function normalizeExpiry($expiry)
+    {
+        if ($expiry instanceof DateTime) {
+            return $expiry;
+        }
+        if (! $expiry) {
+            return new DateTime('+1000years');
+        }
+        if (is_numeric($expiry)) {
+            return new DateTime(intval($expiry) . 'sec');
+        }
+        throw new Exception('Unknown expiry format. Integer value or DateTime object was expected.');
+    }
+
+    protected function isExpired($expiry)
+    {
+        return $this->normalizeExpiry($expiry) < (new DateTime);
+    }
+}
